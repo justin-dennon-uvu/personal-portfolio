eca3ff332fef138bb04116b68aecfd0f74f0a764
Justin <10875645@my.uvu.edu>
2021-04-02 22:35:26 -0600

    added "all" and "missed votes" buttons

--- senators/main.js
+++ senators/main.js
@@ -1,22 +1,56 @@
-import { senators } from '../data/senators.js'
+import { senators } from "../data/senators.js";
+import { clearChildren } from "../utils/main.js";
 
-const nav = document.querySelector('nav')
-const main = document.querySelector('main')
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+
+const allButton = document.createElement("button");
+allButton.textContent = "All Senators";
+allButton.addEventListener("click", () => populateMain(senators));
+nav.appendChild(allButton);
+
+const missedButton = document.createElement("button");
+missedButton.textContent = "Total Missed Votes";
+missedButton.addEventListener("click", () => populateMissed(totalMissed));
+nav.appendChild(missedButton);
 
 function populateMain(senators) {
-    senators.forEach(senator => {
+  clearChildren(main);
+  senators.forEach((senator) => {
+    let senatorFigure = document.createElement("figure");
+    let senatorImg = document.createElement("img");
+    let senatorCaption = document.createElement("figcaption");
 
-        let senatorFigure = document.createElement('figure')
-        let senatorImg = document.createElement('img')
-        let senatorCaption = document.createElement('figcaption')
+    senatorImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`;
+    senatorCaption.textContent = senator.first_name;
 
-        senatorImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`
-        senatorCaption.textContent = senator.first_name
-
-        senatorFigure.appendChild(senatorImg)
-        senatorFigure.appendChild(senatorCaption)
-        main.appendChild(senatorFigure)
-    })
+    senatorFigure.appendChild(senatorImg);
+    senatorFigure.appendChild(senatorCaption);
+    main.appendChild(senatorFigure);
+  });
 }
 
-populateMain(senators)
+function populateMissed(totalMissed) {
+  clearChildren(main);
+  totalMissed.forEach((senator) => {
+    let missedFigure = document.createElement("figure");
+    let missedImg = document.createElement("img");
+    let missedCaption = document.createElement("figcaption");
+
+    missedImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`;
+    missedCaption.textContent =
+      senator.first_name + " " + senator.missed_votes;
+
+    missedFigure.appendChild(missedImg);
+    missedFigure.appendChild(missedCaption);
+    main.appendChild(missedFigure);
+  });
+}
+
+let totalMissed = senators.filter((senator) => senator.missed_votes);
+let missedArray = totalMissed.map((senator) => senator.missed_votes);
+missedArray.sort();
+
+populateMain(senators);
+console.log(missedArray);
+// apply missedArray to each senator in descending order
