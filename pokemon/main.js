87c96dc7ba8a9df4135150486ab3bd02d09dc4dc
Justin <10875645@my.uvu.edu>
2021-04-21 19:19:46 -0600

    closer to finishing constructor

--- pokemon/main.js
+++ pokemon/main.js
@@ -2,10 +2,13 @@ const cardView = document.querySelector(".card-view");
 
 const newButton = document.querySelector(".custom-pokemon");
 
+{/* <img class="type-img" style="width: 16px;" src="../images/${pokemon.types[0].type.name}.svg"></img> */}
+
 class Pokemon {
-  constructor(name, height, weight, abilities, moves) {
+  constructor(name, type, height, weight, abilities, moves) {
     this.id = 899;
     this.name = name;
+    this.type = type;
     this.height = height;
     this.weight = weight;
     this.abilities = abilities;
@@ -15,10 +18,12 @@ class Pokemon {
 
 newButton.addEventListener("click", () => {
   let pokeName = prompt("What is the name of your new Pokemon?");
+  let pokeType = prompt("Pokemon type?")
   let pokeHeight = prompt("Pokemon height?");
   let pokeWeight = prompt("Pokemon weight?");
   let newPokemon = new Pokemon(
     pokeName,
+    pokeType,
     pokeHeight,
     pokeWeight,
     ["eat", "sleep"],
@@ -67,12 +72,17 @@ function populateCardFront(pokemon) {
   let cardFront = document.createElement("div");
   cardFront.className = "card card-front";
   let cardFrontName = document.createElement("div");
-  // cardFrontName.className = "card-front-name";
-  // cardFrontName.innerHTML = `<p>${pokemon.name}</p><img class="type-img" style="width: 16px;" src="../images/${pokemon.types[0].type.name}.svg">`;
+  cardFrontName.className = "card-front-name";
+  let cardFrontNameText = document.createElement("p");
+  cardFrontNameText.textContent = pokemon.name;
+  let cardFrontNameType = document.createElement('img')
+  cardFrontNameType.src = getTypeImage(pokemon)
   let cardFrontImage = document.createElement("img");
-  cardFrontImage.src = getImage(pokemon)
+  cardFrontImage.src = getImage(pokemon);
+  cardFrontName.appendChild(cardFrontNameText);
+  cardFrontName.appendChild(cardFrontNameType)
   cardFront.appendChild(cardFrontImage);
-  // cardFront.appendChild(cardFrontName);
+  cardFront.appendChild(cardFrontName);
   return cardFront;
 }
 
@@ -86,12 +96,22 @@ function populateCardBack(pokemon) {
 }
 
 function getImage(pokemon) {
-  let pokeID
-  if (pokemon.id < 899) pokeID = pokemon.id
+  let pokeID;
+  if (pokemon.id < 899) pokeID = pokemon.id;
   if (pokemon.id === 899) {
-    return `../images/pokeball.png`
+    return `../images/pokeball.png`;
   }
   return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokeID}.png`;
 }
 
-populateCardview()
+function getTypeImage(pokemon) {
+  let typeImg
+  if (pokemon.id < 899) {
+    return `../images/${pokemon.types[0].type.name}.svg`
+  }
+  if (pokemon.id === 900) {
+    return `../images/${pokeType}.svg`
+  }
+}
+
+populateCardview();
