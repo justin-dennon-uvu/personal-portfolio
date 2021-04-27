6012ae017262c0803c1ec05f3f6256f35f400667
Justin <10875645@my.uvu.edu>
2021-04-27 16:00:50 -0600

    started name prefix

--- senators/main.js
+++ senators/main.js
@@ -14,9 +14,30 @@ allButton.textContent = "All Senators";
 const repubButton = document.createElement("button");
 nav.appendChild(repubButton);
 repubButton.addEventListener("click", () => {
-  populateMain(filterSenators(senators, "R"));
+  populateMain(filterParty(senators, "R"));
 });
 repubButton.textContent = "Republicans";
+
+const demButton = document.createElement("button");
+nav.appendChild(demButton);
+demButton.addEventListener("click", () => {
+  populateMain(filterParty(senators, "D"));
+});
+demButton.textContent = "Democrats";
+
+const femaleButton = document.createElement('button')
+nav.appendChild(femaleButton)
+femaleButton.addEventListener('click', () => {
+  populateMain(filterGender(senators, "F"))
+})
+femaleButton.textContent = 'Female Senators'
+
+const maleButton = document.createElement('button')
+nav.appendChild(maleButton)
+maleButton.addEventListener('click', () => {
+  populateMain(filterGender(senators, "M"))
+})
+maleButton.textContent = 'Male Senators'
 
 function populateMain(senators) {
   clearChildren(main);
@@ -25,8 +46,8 @@ function populateMain(senators) {
     let senatorImg = document.createElement("img");
     let senatorCaption = document.createElement("figcaption");
 
-    senatorImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`;
-    senatorCaption.textContent = senator.first_name;
+    senatorImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`;
+    senatorCaption.textContent = namePrefix(senators, `M`) + " " + senator.first_name;
 
     senatorFigure.appendChild(senatorImg);
     senatorFigure.appendChild(senatorCaption);
@@ -34,8 +55,21 @@ function populateMain(senators) {
   });
 }
 
-const filterSenators = (senators, politicalParty) => {
+const filterParty = (senators, politicalParty) => {
   return senators.filter((member) => member.party === politicalParty);
 };
+
+const filterGender = (senators, senatorGender) => {
+  return senators.filter((member) => member.gender === senatorGender);
+};
+
+const namePrefix = (senators, senatorGender) => {
+  if (senatorGender === 'F') {
+    return 'Mrs.'
+  }
+  if (senatorGender === 'M') {
+    return 'Mr.'
+  }
+}
 
 populateMain(senators);
