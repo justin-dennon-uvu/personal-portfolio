2940d610728b7f929807bf418ea0d2614e8395f7
Justin <10875645@my.uvu.edu>
2021-04-27 14:28:45 -0600

    all senators set to refresh

--- senators/main.js
+++ senators/main.js
@@ -6,7 +6,7 @@ const main = document.querySelector("main");
 
 const allButton = document.createElement("button");
 allButton.textContent = "All Senators";
-allButton.addEventListener("click", () => populateMain(senators));
+allButton.addEventListener("click", () => document.location.reload(true));
 nav.appendChild(allButton);
 
 const missedButton = document.createElement("button");
@@ -15,6 +15,7 @@ missedButton.addEventListener("click", () => missedSort());
 nav.appendChild(missedButton);
 
 function populateMain(senators) {
+  
   clearChildren(main);
   senators.forEach((senator) => {
     let senatorFigure = document.createElement("figure");
@@ -22,7 +23,7 @@ function populateMain(senators) {
     let senatorCaption = document.createElement("figcaption");
 
     senatorImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`;
-    senatorCaption.textContent = senator.first_name + " " + senator.missed_votes;
+    senatorCaption.textContent = senator.first_name
 
     senatorFigure.appendChild(senatorImg);
     senatorFigure.appendChild(senatorCaption);
@@ -34,23 +35,5 @@ function missedSort() {
   clearChildren(main);
   populateMain((senators).sort((a, b) => a.missed_votes - b.missed_votes).reverse())
 }
-
-// function populateMissed(totalMissed) {
-//   clearChildren(main);
-//   totalMissed.forEach((senator) => {
-//     let missedFigure = document.createElement("figure");
-//     let missedImg = document.createElement("img");
-//     let missedCaption = document.createElement("figcaption");
-
-//     missedImg.src = `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`;
-//     missedCaption.textContent = senator.first_name + " " + senator.missed_votes;
-
-//     missedFigure.appendChild(missedImg);
-//     missedFigure.appendChild(missedCaption);
-//     main.appendChild(missedFigure);
-//   });
-// }
-
-// let totalMissed = senators.filter((senator) => senator.missed_votes);
 
 populateMain(senators);
